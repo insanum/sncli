71b053d762a332359dbb083df3438ff4c7b943dc
Eric Davis <edavis@insanum.com>
2015-04-02 16:24:36 -0700

    EDITOR and PAGER environment variables override .snclirc config

--- simplenote_cli/sncli.py
+++ simplenote_cli/sncli.py
@@ -60,7 +60,7 @@ class sncli:
 
     def get_editor(self):
         editor = self.config.get_config('editor')
-        if not editor and os.environ['EDITOR']:
+        if os.environ['EDITOR']:
             editor = os.environ['EDITOR']
         if not editor:
             self.log(u'No editor configured!')
@@ -69,7 +69,7 @@ class sncli:
 
     def get_pager(self):
         pager = self.config.get_config('pager')
-        if not pager and os.environ['PAGER']:
+        if os.environ['PAGER']:
             pager = os.environ['PAGER']
         if not pager:
             self.log(u'No pager configured!')
